36962de96fc6271674c2ed0726989ae6d65ee517
rahfimuhammad <rahfi.muhammad@alumni.ui.ac.id>
2025-02-03 15:20:08 +0700

    update db

--- db/index.js
+++ db/index.js
@@ -2,4 +2,17 @@ const { PrismaClient } = require("@prisma/client")
 
 const prisma = new PrismaClient()
 
+async function checkConnection() {
+    try {
+      await prisma.$connect();
+      console.log("Database connected successfully!");
+    } catch (error) {
+      console.error("Failed to connect to the database:", error);
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+  
+  checkConnection();
+
 module.exports = prisma
